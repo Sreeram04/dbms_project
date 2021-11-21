427aa4a69302339291cec006bd2c8b08d4309ad2
Sreeram04 <89728553+Sreeram04@users.noreply.github.com>
2021-11-21 20:43:30 +0530

    Update sqlconnnectprct1.py

--- sqlconnnectprct1.py
+++ sqlconnnectprct1.py
@@ -1,8 +1,17 @@
 import mysql.connector as msc
 import tkinter as tk
+import tkinter.messagebox as tkmsg
 from tkinter import *
 
-pswd_dict={'9931':"RLL13",'9983':"HFY54"}
+import mysql.connector as ms
+mydb =  ms.connect(host="localhost",user="root",passwd="system",database="HotelDB")
+cur = mydb.cursor()
+cur.execute("Select * from password_hotel")
+data=cur.fetchall()
+pswd_dict={}
+for i in data:
+	pswd_dict[str(i[0])]=i[1]
+print(pswd_dict)
 
 def customer():
 	cust_win=tk.Toplevel(win)
@@ -13,6 +22,61 @@ def customer():
 	cust_label.pack()
 	cust_exit.pack()
 
+def room_view():
+	con=msc.connect(host="localhost",user="root",passwd="system",database="HotelDB")
+	curr=con.cursor()
+	curr.execute("select * from room where room_status='Available'")
+	rows=curr.fetchall()
+	List.delete(0,List.size())
+	data="room_no   room_id    room_status"
+	List.insert(List.size()+1,data)
+	for i in rows:
+		data=str(i[0])+"              "+str(i[1])+"            "+i[2]
+		List.insert(List.size()+1, data)
+	con.close()
+
+def room_update():
+	room_no=i_room_no.get()
+	room_status=i_room_status.get()
+	if(room_no=="" or room_status==""):
+		tkmsg.showinfo("Update Status","Field is Mandatory")
+	else:
+		con=msc.connect(host="localhost",user="root",password="system",database="HotelDB")
+		curr=con.cursor()
+		curr.execute("update room set room_status='"+ room_status +"' where room_no='"+ room_no +"'")
+		curr.execute("commit")
+		i_room_no.delete(0,END)
+		i_room_status.delete(0,END)
+		tkmsg.showinfo("Update Status","Updation Successfull")
+		con.close()
+
+def room():
+	global room_win
+	room_win=tk.Toplevel(staff_op)
+	room_win.geometry('700x400')
+	room_win.resizable(width=False,height=False)
+
+	global i_room_no
+	global i_room_status
+	room_no=IntVar
+	room_status=StringVar
+	t_room_no=tk.Label(room_win,text='Enter room_no')
+	t_room_no.place(x=20,y=30)
+	i_room_no=tk.Entry(room_win,textvariable=room_no)
+	i_room_no.place(x=150,y=30)
+	t_room_status=tk.Label(room_win,text='Enter room status')
+	t_room_status.place(x=20,y=60)
+	i_room_status=tk.Entry(room_win,textvariable=room_status)
+	i_room_status.place(x=150,y=60)
+
+	roomview=tk.Button(room_win,text='room details',command=room_view)
+	roomview.place(x=20,y=140)
+	roomupdate=tk.Button(room_win,text='room update',comman=room_update)
+	roomupdate.place(x=120,y=140)
+	global List
+	List=tk.Listbox(room_win,height=20,width=30)
+	List.place(x=500,y=30)
+
 def staff_operations():
 	global staff_op
 	staff_op=tk.Toplevel(staff_win)
@@ -20,6 +84,8 @@ def staff_operations():
 	staff_op.resizable(width=False,height=False)
 	staff_oper=tk.Label(staff_op,text="Do staff operations here")
 	staff_oper.pack()
+	room_button=tk.Button(staff_op,text="room details",command=room)
+	room_button.pack()
 	staff_exit=tk.Button(staff_op,text="Exit",command=staff_op.destroy)
 	staff_exit.pack()
 
@@ -31,7 +97,7 @@ def login_verify():
 	if user1 in pswd_dict and pswd1==pswd_dict[user1]:
 		staff_operations()
 	else:
-		verify=Label(staff_win,text="Incorrect staff id or password")
+		verify=Label(staff_win,text="Incorrect staff_id or password")
 		try_again=Label(staff_win,text="Try Again")
 		verify.pack()
 		try_again.pack()
