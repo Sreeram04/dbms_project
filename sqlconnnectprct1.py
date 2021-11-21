d70f83a5e3346995ec00fd3eb1e7f0b5fab80cd2
Sreeram04 <89728553+Sreeram04@users.noreply.github.com>
2021-11-21 14:46:25 +0530

    Update sqlconnnectprct1.py

--- sqlconnnectprct1.py
+++ sqlconnnectprct1.py
@@ -1,23 +1,67 @@
-
+import mysql.connector as msc
 import tkinter as tk
+from tkinter import *
+
+pswd_dict={'9931':"RLL13",'9983':"HFY54"}
 
 def customer():
 	cust_win=tk.Toplevel(win)
 	cust_win.geometry('600x400')
 	cust_win.resizable(width=False,height=False)
-	cust_label=tk.Label(cust_win,text="This is the customer page")
+	cust_label=tk.Label(cust_win,text="Do customer operations here")
 	cust_exit=tk.Button(cust_win,text="Exit",command=cust_win.destroy)
 	cust_label.pack()
 	cust_exit.pack()
 
+def staff_operations():
+	global staff_op
+	staff_op=tk.Toplevel(staff_win)
+	staff_op.geometry('600x400')
+	staff_op.resizable(width=False,height=False)
+	staff_oper=tk.Label(staff_op,text="Do staff operations here")
+	staff_oper.pack()
+	staff_exit=tk.Button(staff_op,text="Exit",command=staff_op.destroy)
+	staff_exit.pack()
+
+def login_verify():
+	user1=user.get()
+	pswd1=pswd.get()
+	user_entry.delete(0, END)
+	pswd_entry.delete(0, END)
+	if user1 in pswd_dict and pswd1==pswd_dict[user1]:
+		staff_operations()
+	else:
+		verify=Label(staff_win,text="Incorrect staff id or password")
+		try_again=Label(staff_win,text="Try Again")
+		verify.pack()
+		try_again.pack()
+
 def staff():
+	global staff_win
 	staff_win=tk.Toplevel(win)
 	staff_win.geometry('600x400')
 	staff_win.resizable(width=False,height=False)
-	staff_label=tk.Label(staff_win,text="This is the staff page")
+	#staff_label=tk.Label(staff_win,text="This is the staff page")
+	global user
+	global pswd
+	global user_entry
+	global pswd_entry
+	user=StringVar()
+	pswd=StringVar()
+	user_label=tk.Label(staff_win,text='Username ')
+	user_label.pack()
+	user_entry=tk.Entry(staff_win,textvariable=user)
+	user_entry.pack()
+	pswd_label=tk.Label(staff_win,text='Password ')
+	pswd_label.pack()
+	pswd_entry=tk.Entry(staff_win,textvariable=pswd,show='*')
+	pswd_entry.pack()
+	login_button=tk.Button(staff_win,text='Log in',command=login_verify)
+	login_button.pack()
 	staff_exit=tk.Button(staff_win,text="Exit",command=staff_win.destroy)
-	staff_label.pack()
+	#staff_label.pack()
 	staff_exit.pack()
+
 
 win=tk.Tk()
 win.geometry('600x400')
