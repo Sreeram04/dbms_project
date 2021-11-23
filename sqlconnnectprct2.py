a0819bbd84cd9a9142fcc82dbda480cb0d71a09f
Sreeram04 <89728553+Sreeram04@users.noreply.github.com>
2021-11-23 15:18:51 +0530

    Update sqlconnnectprct2.py

--- sqlconnnectprct2.py
+++ sqlconnnectprct2.py
@@ -15,23 +15,19 @@ def customer():
 	cust_exit.pack()
 
 def c_insert():
-	e_cid=StringVar()
-	e_cfname=StringVar()
-	e_clname=StringVar()
-	e_crno=StringVar()
-	e_cphno=StringVar()
-	e_cstat=StringVar()
-	cid=e_cid.get()
-	cfname=e_cfname.get()
-	clname=e_clname.get()
-	crno=e_crno.get()
-	cphno=e_cphno.get()
-	cstat=e_cstat.get()
+
+	cid=i_cid.get()
+	cfname=i_cfname.get()
+	clname=i_clname.get()
+	crno=i_crno.get()
+	cphno=i_cphno.get()
+	cstat=i_cstat.get()
 	print("1",cid,"2",cfname,"3",clname,"4",crno,"5",cphno,"6",cstat)
+	'''
 	if (cid=="" or cfname=="" or crno=="" or clname=="" or cphno=="" or cstat==""):
 		tkmsg.showinfo("Insert Status","All Fields are Mandatory")
 	else:
-		con=msc.connect(host="localhost",user="root",password="Password123#@!",database="HotelDB")
+		con=msc.connect(host="localhost",user="root",password="system",database="HotelDB")
 		curr=con.cursor()
 		curr.execute("insert into customer values('"+ cid +"','"+ cfname +"','"+ clname +"','"+ crno +"','"+ cphno +"','"+ cstat +"')")
 		curr.execute("commit")   
@@ -43,7 +39,7 @@ def c_insert():
 		e_cstat.delete(0,'end')
 		tkmsg.showinfo("Insert Status","Insertion Successfull")
 		con.close()
-
+		'''
 def c_delete():
 	print("H")
 
@@ -58,31 +54,54 @@ def staff_operations():
 	staff_oper=tk.Label(staff_op,text="Customer Details",font=('Helvetica 15 underline')).place(x=20,y=20)
 	#staff_oper.pack()
 	#Customer Details
-	global e_cid
-	global e_cfname
-	global e_clname
-	global e_crno
-	global e_cphno
-	global e_cstat
-	cid=tk.IntVar()
+	global i_cid
+	global i_cfname
+	global i_clname
+	global i_crno
+	global i_cphno
+	global i_cstat
+	#cid=tk.IntVar()
 	#e_cid=tk.IntVar()
-	cfname=StringVar()
-	cid=tk.Label(staff_op,text='Customer ID',font=("bold",10)).place(x=20,y=50)
-	e_cid=tk.Entry(staff_op,textvariable=cid).place(x=150,y=50)
-	cfname=tk.Label(staff_op,text='First Name',font=("bold",10)).place(x=20,y=80)
-	e_cfname=tk.Entry(staff_op).place(x=150,y=80)
-	clname=tk.Label(staff_op,text='Last Name',font=("bold",10)).place(x=20,y=110)
-	e_clname=tk.Entry(staff_op).place(x=150,y=110)
-	crno=tk.Label(staff_op,text='Room No:',font=("bold",10)).place(x=20,y=140)
-	e_crno=tk.Entry(staff_op).place(x=150,y=140)
-	cphno=tk.Label(staff_op,text='Phone No:',font=("bold",10)).place(x=20,y=170)
-	e_cphno=tk.Entry(staff_op).place(x=150,y=170)
-	cstat=tk.Label(staff_op,text='Status (In/Out)',font=("bold",10)).place(x=20,y=200)
-	e_cstat=tk.Entry(staff_op).place(x=150,y=200)
-	insert=tk.Button(staff_op,text="Insert Customer",command=c_insert).place(x=20,y=250)
-	delete=tk.Button(staff_op,text="Delete Customer",command=c_delete).place(x=180,y=250)
-	update=tk.Button(staff_op,text="Update Customer",command=c_update).place(x=340,y=250)		
-	staff_exit=tk.Button(staff_op,text="Exit",command=staff_op.destroy).place(x=520,y=360)
+	#cfname=StringVar()
+	e_cid=StringVar
+	e_cfname=StringVar
+	e_clname=StringVar
+	e_crno=StringVar
+	e_cphno=StringVar
+	e_cstat=StringVar
+
+	cid=tk.Label(staff_op,text='Customer ID',font=("bold",10))
+	cid.place(x=20,y=50)
+	i_cid=tk.Entry(staff_op,textvariable=e_cid)
+	i_cid.place(x=150,y=50)
+	cfname=tk.Label(staff_op,text='First Name',font=("bold",10))
+	cfname.place(x=20,y=80)
+	i_cfname=tk.Entry(staff_op,textvariable=e_cfname)
+	i_cfname.place(x=150,y=80)
+	clname=tk.Label(staff_op,text='Last Name',font=("bold",10))
+	clname.place(x=20,y=110)
+	i_clname=tk.Entry(staff_op,textvariable=e_clname)
+	i_clname.place(x=150,y=110)
+	crno=tk.Label(staff_op,text='Room No:',font=("bold",10))
+	crno.place(x=20,y=140)
+	i_crno=tk.Entry(staff_op,textvariable=e_crno)
+	i_crno.place(x=150,y=140)
+	cphno=tk.Label(staff_op,text='Phone No:',font=("bold",10))
+	cphno.place(x=20,y=170)
+	i_cphno=tk.Entry(staff_op,textvariable=e_cphno)
+	i_cphno.place(x=150,y=170)
+	cstat=tk.Label(staff_op,text='Status (In/Out)',font=("bold",10))
+	cstat.place(x=20,y=200)
+	i_cstat=tk.Entry(staff_op,textvariable=e_cstat)
+	i_cstat.place(x=150,y=200)
+	insert=tk.Button(staff_op,text="Insert Customer",command=c_insert)
+	insert.place(x=20,y=250)
+	delete=tk.Button(staff_op,text="Delete Customer",command=c_delete)
+	delete.place(x=180,y=250)
+	update=tk.Button(staff_op,text="Update Customer",command=c_update)
+	update.place(x=340,y=250)		
+	staff_exit=tk.Button(staff_op,text="Exit",command=staff_op.destroy)
+	staff_exit.place(x=520,y=360)
 
 def login_verify():
 	user1=user.get()
